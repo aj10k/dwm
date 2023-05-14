8516c180410b8eddfaae6904e9799ea6fc7f1b10
aj10k <28464125+aj10k@users.noreply.github.com>
2023-05-14 16:12:30 -0400

    gaps

--- config.h
+++ config.h
@@ -6,17 +6,17 @@
 #define BROWSER "librewolf"
 
 /* appearance */
-static unsigned int borderpx  = 5;        /* border pixel of windows */
+static unsigned int borderpx  = 8;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int gappih    = 40;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 60;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=28:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -334,3 +334,4 @@ static const Button buttons[] = {
 	{ ClkTagBar,		0,		Button5,	shiftview,	{.i = 1} },
 	{ ClkRootWin,		0,		Button2,	togglebar,	{0} },
 };
+
